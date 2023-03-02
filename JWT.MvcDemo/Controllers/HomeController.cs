474576a8261af49380d402c7b024d2dd5b7c5fce
Maliang.China <42198953+jidanji@users.noreply.github.com>
2023-03-02 16:37:47 +0800

    111
    11

---
+++ JWT.MvcDemo/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using JWT.Common;
+using JWT.MvcDemo.App_Start;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace JWT.MvcDemo.Controllers
+{
+
+  
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            Log4Help.Info("1111");
+            Log4Help.Error("12345",null);
+            return View();
+        }
+
+        [MyAuthorize]
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return Json("Your application description page.",JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+
+        public ActionResult Error()
+        {
+            return Json("权限不足", JsonRequestBehavior.AllowGet);
+        }
+
+    }
+}
